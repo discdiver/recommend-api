e30da42875d8f790e938d29b9c54b297c4064e48
Jeff Hale <jeffmshale@gmail.com>
2021-10-23 10:18:34 -0400

    working product recommender server

--- serve.py
+++ serve.py
@@ -1,14 +1,24 @@
-from compute_similar import get_cosine_df
+import pandas as pd
 from fastapi import FastAPI
+
+
+
+cos_df = pd.read_pickle('similarities.pkl')
 
 app = FastAPI()
 
-@app.get('products/{productid}')
-async def get_similar_prods(productid: str):
+@app.get('/products/{productid}')
+async def get_similar_prods(productid: int):
     try:
-        return get_cosine_df(productid)
+        
+        top3 = cos_df.loc[:, 111633].nlargest(4)[1:]
+        return top3
     except Exception:
         print(
-            """Sorry, there was an error. 
+            """Sorry, there was an error. {Exception}
             Are you sure that Product ID exists?"""
         )
+
+# TODO add logging
+# TODO add product text
+# TODO require product names to be a max length
