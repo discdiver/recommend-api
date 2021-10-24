3ee779d604568d3481c39e0cf13f1a6752dbf88e
Jeff Hale <jeffmshale@gmail.com>
2021-10-24 14:34:51 -0400

    add requirements.txt and provide functional api

--- serve.py
+++ serve.py
@@ -2,23 +2,29 @@ import pandas as pd
 from fastapi import FastAPI
 
 
-
 cos_df = pd.read_pickle('similarities.pkl')
 
 app = FastAPI()
 
 @app.get('/products/{productid}')
 async def get_similar_prods(productid: int):
+    """return the products most similar to the product id given
+
+    Args:
+        productid: the productid to find similar products with
+    Returns:
+        the 3 most similar products
+    """
+    
     try:
-        
-        top3 = cos_df.loc[:, 111633].nlargest(4)[1:]
-        return top3
+        return cos_df.loc[:, productid].nlargest(4)[1:]
     except Exception:
         print(
             """Sorry, there was an error. {Exception}
             Are you sure that Product ID exists?"""
         )
 
-# TODO add logging
+# TODO add tests
 # TODO add product text
-# TODO require product names to be a max length
+# TODO require product id to be a positive integer
+# TODO add logging
