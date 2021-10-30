f25d1068f1c3ac4a1e1122ce36d67c5c2f35240b
Jeff Hale <jeffmshale@gmail.com>
2021-10-30 16:36:28 -0400

    basic formatting on home page

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
 @app.get('/', response_class=HTMLResponse)
-async def home(request: Request):
+def home(request: Request):
     return templates.TemplateResponse("index.html", {'request': request})
 
 @app.get('/products/{productid}')
