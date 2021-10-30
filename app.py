9b88534868d5c1156daaa02fcf44f3b8d5313b68
Jeff Hale <jeffmshale@gmail.com>
2021-10-30 15:59:12 -0400

    add basic template for home page

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Request
+from fastapi.templating import Jinja2Templates
+# from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 
 cos_df = pd.read_pickle('similarities.pkl')
 
 app = FastAPI()
 
+# app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
+
 @app.get('/products/{productid}')
-async def get_similar_prods(productid: int):
+async def get_similar_prods(productid: int=Path(..., title="The ID of the product", gt=1)):
     """return the products most similar to the product id given
 
     Args:
@@ -23,8 +34,7 @@ async def get_similar_prods(productid: int):
             """Sorry, there was an error. {Exception}
             Are you sure that Product ID exists?"""
         )
+    
 
-# TODO add tests
 # TODO add product text
-# TODO require product id to be a positive integer
 # TODO add logging
